36bbf7a475b33e51903d04e60d2511d8e14b46e4
Sasha61077 <maletskiya@ukr.net>
2022-07-15 00:55:18 +0300

    Java scripts

---
+++ ДЗ16/JavaScript/J_Script_01.js
@@ -0,0 +1,56 @@
+/*console.log("Задача №1");
+let a = 11;
+let b = 4;
+let division_result = a%b;
+if (division_result === 0){console.log("Делится. Резульат деления = " + (a/b));
+} else if (division_result != 0) {console.log ("Делится с остатком. Остаток от деления =" +  division_result);
+}*/
+
+
+
+/*console.log("Задача №2");
+let a = prompt("Введи высоту треугольника");
+let b = prompt("Введи символ, которым будет нарисован треугольник");
+let c = b;
+function triangle (){
+	a=+a;
+	while(a>0){
+		console.log(b);
+		b = b + c;
+		a = a - 1;
+	}
+}
+triangle();*/
+
+
+
+/*console.log("Задача №3 вариант№1");
+for (let x = 0; x<=100; x++){
+	if(x % 2 === 0) {
+		continue;
+		}
+	console.log(x)
+}*/
+
+/*console.log("Задача №3 вариант№2");
+let i = 0;
+while(i<=100){
+	let a = i/2;
+	let b = Math.round(a);
+	if (a != b) {console.log(i);
+	}
+	i = ++i;
+}*/
+
+
+
+/*console.log("Задача №4");
+let n = 1000;
+let x = 0;
+while(n>50){
+	n = n / 2;
+	x = x + 1;
+	}
+	console.log("Получится число", n);
+	console.log("Количество итераций", x);
+*/
