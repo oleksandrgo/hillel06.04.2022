182f1d26d780f7bdfa6a74d4128ceb72a5a70db9
Frutl202 <danyky.martynov@gmail.com>
2022-07-16 00:09:35 +0300

    DZ16

---
+++ ДЗ16/JavaScript/JavaScript.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log( i + 1 );
+//   }
+// }
+
+
+
+// let x = 0, y = 0;
+// let max = 7;   
+// while (x < max) { 
+//     DrawTriangle = "";   
+//     for (y = 0; y < x + 1; y++) DrawTriangle += "*";
+//     console.log(DrawTriangle);
+//     x++;
+// }
+
+
+
+// for (var num = 1000, i = 0; num > 50; num = num / 2, i++);
+// console.log(num, i);
+
+
+// let a = 20;
+// let b = 6;
+// let rest = a % b;
+// if (rest != 0) {
+//   console.log('Делится с остатком ' + rest);
+// } else {
+//   console.log('Делится, ' + (a / b));
+// }
