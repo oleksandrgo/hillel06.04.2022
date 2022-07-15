0d92f3677bd284c6bdd8c14a0f74b92ee5279232
avborsch <av.borsch@gmail.com>
2022-07-15 23:14:41 +0300

    DZ16

---
+++ DZ16/HomeWork16JS.js
@@ -0,0 +1,70 @@
+/*console.log("Task1");
+var a;
+var b;
+a = 115;
+b = 5;
+var c = a/b - Math.floor(a/b);
+if (c > 0) {
+	console.log("Делится с остатком");
+	console.log("Остаток = " + Math.ceil( 1/c ));
+} else {
+	console.log("Делится");
+    console.log("Результат = " + a/b);
+}
+
+console.log("--------------------------");*/
+
+/*console.log("Task1 - Varian2");
+var a;
+var b;
+a = 115;
+b = 4;
+var c = a%b;
+if (c != 0) {
+	console.log("Делится с остатком");
+	console.log("Остаток = " + c);
+} else {
+	console.log("Делится");
+    console.log(a/b);
+}
+
+console.log("----------------------------");*/
+
+/*console.log("Task2");
+
+var lines = 10;
+var row = " ";
+var simbol = "*";
+function printTriangle(lines, simbol) {for (var i = 0; i < lines; i++) {
+  row += simbol;
+  console.log(row);
+}
+}
+printTriangle(lines, simbol);
+
+
+console.log("----------------------------")*/
+
+/*console.log("Task3");
+
+let maxNumber = 100;
+for (let a = 0; a < maxNumber; a++) {
+  if (a % 2) {
+    console.log( a );
+  }
+
+}
+
+console.log("----------------------------")*/
+
+
+console.log("Task4");
+
+let n = 1000;
+let numberIteration = 0;
+do {
+	n = n / 2;
+	numberIteration++;
+} while (n > 50);
+console.log( "Resolt: n = " + n );
+console.log( "NumberIteration: " + numberIteration );
