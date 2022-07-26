2758522a21e12021d965346e8cfd59a7ccacdd76
ALMK3 <slidev33@gmail.com>
2022-07-26 21:00:04 +0300

    ДЗ 17. Написать регулярные выражения

---
+++ dz17/dz17js.js
@@ -0,0 +1,9 @@
+
+/////=======Regular Expressions=======\\\\\
+
+let name = document.getElementsByName("#fname");
+       let nameRegExp = /^([a-zA-Z]{2,20})$/;
+let mail = document.getElementsByName("#mail");
+       let mailRegExp = /^(\w+@gmail\.com||ukr\.net]){6,12}$/;
+let num = document.getElementsByName("#phonenum");
+       let numRegExp = /^([380]+[50]||[63]||[66]||[67]||[68]||[91]||[92]||[93]||[97]+\d {7})$/;
