87538d7b009bb47daef2ef7c74aad236f8dff338
Lidia_Dolhopoliuk <lidasatoko9@gmail.com>
2022-07-29 15:35:06 +0300

    Fixed validation form for name, email, number

--- DreamSite/js/app.js
+++ DreamSite/js/app.js
@@ -6,9 +6,9 @@ document.getElementById('btnfm').addEventListener('click', function() {
 	let userNameValue = userName.value;
 	let userPhoneValue = userPhone.value
 	let userEmailValue = userEmail.value
-	if (userNameValue.match(/^[a-zA-Zа']{2,20}$/) === null && 
-		userEmailValue.match(/^[a-zA-Z]{1,20}@{1}(\bgmail\.com|ukr\.net\b){1}$/) === null &&
-		userPhoneValue.match(/^(\b38050|38063|38066|38067|38068|38091|38092|38093|38096|38097\b){1}\d{7}$/)) {
+	if (userNameValue.match(/^[a-zA-Zа']{2,20}$/) === null || 
+		userEmailValue.match(/^[a-zA-Z]{1,20}@{1}(\bgmail\.com|ukr\.net\b){1}$/) === null ||
+		userPhoneValue.match(/^(380){1}(50|63|66|67|68|91|92|93|96|97){1}\d{7}$/) === null) {
 		alert("Мы вас не слышим");
 		return;
 	} else {
